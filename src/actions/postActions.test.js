d3d6ab215bb63eb79abfa84ecc5d5459eeb990b1
janelle mellor <janellemellor@gmail.com>
2020-05-11 14:49:04 -0700

    add test for add a post action

---
+++ src/actions/postActions.test.js
@@ -0,0 +1,20 @@
+import { ADD_POST, addPost } from './postActions';
+
+describe('post actions', () => {
+  it('creates an ADD_POST action', () => {
+    const post = addPost({
+      title: 'Blogs are better than pogs',
+      body: 'This is my post about why blogs are better than pogs'   
+    });
+
+    expect(action).toEqual({
+      type: ADD_POST,
+      payload: {
+        title: 'Blogs are better than pogs',
+        body: 'This is my post about why blogs are better than pogs'  
+      }
+    });
+  });  
+    
+});
+
