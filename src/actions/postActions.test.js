34b22ce3e77dee8b035a6f1bf2f0fd404c2e358a
janelle mellor <janellemellor@gmail.com>
2020-05-11 14:51:28 -0700

    add passing test for adding a post

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -2,7 +2,7 @@ import { ADD_POST, addPost } from './postActions';
 
 describe('post actions', () => {
   it('creates an ADD_POST action', () => {
-    const post = addPost({
+    const action = addPost({
       title: 'Blogs are better than pogs',
       body: 'This is my post about why blogs are better than pogs'   
     });
