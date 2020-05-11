f26ed426eacbf8adc486b775c8131c08bd8779e7
janelle mellor <janellemellor@gmail.com>
2020-05-11 14:04:02 -0700

    add reducer function to add a post

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,5 +1,15 @@
-// * create `src/reducers/postReducer.js`
 //   * store an array of posts
 //   * handle create a post
 //   * handle delete a post
 //   * BONUS: handle update a post by index
+
+const initialState = [];
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case ADD_POST:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
