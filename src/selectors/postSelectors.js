5f55d582b1e4a0d90633386acfec732b93bdb519
janelle mellor <janellemellor@gmail.com>
2020-05-11 16:12:43 -0700

    add selector test for get all posts

--- src/selectors/postSelectors.js
+++ src/selectors/postSelectors.js
@@ -1,4 +1,7 @@
 // * get all posts
+export const getPosts = state => state;
+
 // * get a post by index
 // * STRETCH: create `src/selectors/commentSelectors.js`
 // * get all comments by post
+
---
+++ src/selectors/postSelectors.test.js
@@ -0,0 +1,36 @@
+// * get all posts
+// * get a post by index
+// * STRETCH: create `src/selectors/commentSelectors.js`
+// * get all comments by post
+
+describe('post selectors', () => {
+  it('gets all posts', () => {
+    const state = [
+      {
+        title: 'Blogs are better than pogs',
+        body: 'This is my post about why blogs are better than pogs' 
+      }, 
+      {
+        title: 'Popcorn and beer',
+        body: 'is an acceptable dinner choice any night of the week'  
+      }
+    ];
+    
+    const posts = getPosts(state);
+
+    expect(posts).toEqual([
+      {
+        title: 'Blogs are better than pogs',
+        body: 'This is my post about why blogs are better than pogs' 
+        }, 
+        {
+        title: 'Popcorn and beer',
+        body: 'is an acceptable dinner choice any night of the week'  
+        }
+    ]);
+  });
+  
+  
+});
+
+
